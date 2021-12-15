353c77fadefb7136ae0e5af14b5dafc289d38878
andreas kirchner <andreas.kirchner@stud.h-da.de>
2021-12-15 18:22:01 +0100

    fix script

--- freebsd_confluent_install.sh
+++ freebsd_confluent_install.sh
@@ -3,21 +3,21 @@ kldload linux && kldload linux64
 pkg install -y emulators/linux_base-c7
 sysrc linux_enable=yes
 #Erstellung von mountpoints die der Emulator benötigt
-echo fdesc   /dev/fd         fdescfs         rw      0       0 >> test.txt
-echo proc    /proc           procfs          rw      0       0 >> test.txt
-echo lineprocfs	/compat/linux/proc	linprocfs	rw	0	0 >> test.txt
-echo linsysfs		/compat/linux/sys	linsysfs		rw	0	0 >> test.txt
-echo tmpfs		/compat/linux/dev/shm	tmpfs	rw,mode=1777		0	0 >>test.txt
+echo fdesc   /dev/fd         fdescfs         rw      0       0 >> /etc/fstab
+echo proc    /proc           procfs          rw      0       0 >> /etc/fstab
+echo lineprocfs	/compat/linux/proc	linprocfs	rw	0	0 >> /etc/fstab
+echo linsysfs		/compat/linux/sys	linsysfs		rw	0	0 >> /etc/fstab
+echo tmpfs		/compat/linux/dev/shm	tmpfs	rw,mode=1777		0	0 >>/etc/fstab
 mount -a
 pkg install -y openjdk8 curl bash
 cd /home && curl -O http://packages.confluent.io/archive/7.0/confluent-7.0.1.tar.gz && tar -xvf confluent-7.0.1.tar.gz && rm -r confluent-7.0.1.tar.gz
-brandelf -t Linux /home/confluent-7.0.0/libexec/cli/linux_amd64/confluent 
-brandelf -t Linux /home/confluent-7.0.0/bin/*
-cd && touch .testrc
-echo PATH=/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/sbin:/usr/local/bin:~/bin:/home/confluent-7.0.1/libexec/cli/linux_amd64/ >> /.testrc
-echo export PATH >> /.testrc
-echo CONFLUENT_HOME=/home/confluent-7.0.1 >> /.testrc
-echo export CONFLUENT_HOME >>/.testrc
+brandelf -t Linux /home/confluent-7.0.1/libexec/cli/linux_amd64/confluent 
+brandelf -t Linux /home/confluent-7.0.1/bin/*
+cd && touch .bashrc
+echo PATH=/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/sbin:/usr/local/bin:~/bin:/home/confluent-7.0.1/libexec/cli/linux_amd64/ >> /.bashrc
+echo export PATH >> /.bashrc
+echo CONFLUENT_HOME=/home/confluent-7.0.1 >> /.bashrc
+echo export CONFLUENT_HOME >>/.bashrc
 ln -s /usr/local/bin/bash /bin/bash
 bash /home/confluent-7.0.1/bin/confluent-hub install --no-prompt confluentinc/kafka-connect-datagen:latest
 cd /home/confluent-7.0.1/bin/ && sed -i '' -e '19i\\freebsd/amd64) found=0 ;;' confluent
