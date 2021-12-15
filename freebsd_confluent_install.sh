12b66aa7d45be776a062352db07a41ab739ef089
andreas kirchner <andreas.kirchner@stud.h-da.de>
2021-12-15 18:29:31 +0100

    fix .bashrc

--- freebsd_confluent_install.sh
+++ freebsd_confluent_install.sh
@@ -14,10 +14,10 @@ cd /home && curl -O http://packages.confluent.io/archive/7.0/confluent-7.0.1.tar
 brandelf -t Linux /home/confluent-7.0.1/libexec/cli/linux_amd64/confluent 
 brandelf -t Linux /home/confluent-7.0.1/bin/*
 cd && touch .bashrc
-echo PATH=/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/sbin:/usr/local/bin:~/bin:/home/confluent-7.0.1/libexec/cli/linux_amd64/ >> /.bashrc
-echo export PATH >> /.bashrc
-echo CONFLUENT_HOME=/home/confluent-7.0.1 >> /.bashrc
-echo export CONFLUENT_HOME >>/.bashrc
+cd && echo PATH=/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/sbin:/usr/local/bin:~/bin:/home/confluent-7.0.1/libexec/cli/linux_amd64/ >> .bashrc
+cd && echo export PATH >> .bashrc
+cd && echo CONFLUENT_HOME=/home/confluent-7.0.1 >> .bashrc
+cd && echo export CONFLUENT_HOME >> .bashrc
 ln -s /usr/local/bin/bash /bin/bash
 bash /home/confluent-7.0.1/bin/confluent-hub install --no-prompt confluentinc/kafka-connect-datagen:latest
 cd /home/confluent-7.0.1/bin/ && sed -i '' -e '19i\\freebsd/amd64) found=0 ;;' confluent
