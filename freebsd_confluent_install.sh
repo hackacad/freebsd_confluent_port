6f3005360478e0a5eb8e796d646d0c9c47ab040b
andreas kirchner <andreas.kirchner@stud.h-da.de>
2021-12-15 19:07:23 +0100

    add comments on the Script

--- freebsd_confluent_install.sh
+++ freebsd_confluent_install.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
+#Laden des Linux Kernel Modules und installation des emulator Pakets
 kldload linux && kldload linux64
 pkg install -y emulators/linux_base-c7
 sysrc linux_enable=yes
-#Erstellung von mountpoints die der Emulator benötigt
+#Erstellung und mount der vom Emulator benoetigten Mountpoints
 echo fdesc   /dev/fd         fdescfs         rw      0       0 >> /etc/fstab
 echo proc    /proc           procfs          rw      0       0 >> /etc/fstab
 echo lineprocfs	/compat/linux/proc	linprocfs	rw	0	0 >> /etc/fstab
 echo linsysfs		/compat/linux/sys	linsysfs		rw	0	0 >> /etc/fstab
 echo tmpfs		/compat/linux/dev/shm	tmpfs	rw,mode=1777		0	0 >>/etc/fstab
 mount -a
+#Installation und Konfiguration von Confluent-7.0.1
 pkg install -y openjdk8 curl bash
 cd /home && curl -O http://packages.confluent.io/archive/7.0/confluent-7.0.1.tar.gz && tar -xvf confluent-7.0.1.tar.gz && rm -r confluent-7.0.1.tar.gz
 brandelf -t Linux /home/confluent-7.0.1/libexec/cli/linux_amd64/confluent 
